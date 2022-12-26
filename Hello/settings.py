f94d23875aee398ddf24682dc0bc6febe339307c
Umar <umartanwar2753@gmail.com>
2022-12-26 23:25:25 +0530

    first commit

--- Hello/settings.py
+++ Hello/settings.py
@@ -108,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'UTC'
-
+    
 USE_I18N = True
 
 USE_TZ = True
@@ -117,7 +117,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
@@ -129,6 +129,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Added Manually
 STATICFILES_DIRS = [
-    BASE_DIR / "static"
+    os.path.join(BASE_DIR / "static"),
+    
 ]
 STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn HELLO.wsgi --log-file -
+web: gunicorn Hello.wsgi --log-file -
