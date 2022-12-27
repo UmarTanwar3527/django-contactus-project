e426f869c47acb3ba3b0e366befcaeb95429a59d
Umar <umartanwar2753@gmail.com>
2022-12-27 21:38:26 +0530

    edit settings for csrf

--- Hello/settings.py
+++ Hello/settings.py
@@ -136,3 +136,8 @@ STATICFILES_DIRS = [
 STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
 MEDIA_URL =  '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
+CSRF_TRUSTED_ORIGINS = [
+    'https://djangoprojectcontactus.up.railway.app'
+]
