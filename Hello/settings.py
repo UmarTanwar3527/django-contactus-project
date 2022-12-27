bf19940cf6e2a76ee8b0f97eb6fd7fe29381b352
Umar <umartanwar2753@gmail.com>
2022-12-27 21:11:37 +0530

    edit settings

--- Hello/settings.py
+++ Hello/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-70k@7jito4^*8m2(&bfgrbw5#o2hk%1*qjrsecr!y#hpejimz#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
